ce1dd76bb59c556304d734e3e58c1cc924c73b96
MargoDeleeck <s090130@ap.be>
2017-08-14 13:53:51 +0200

    verbetering herexamen

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ var app = express();
 
 app.set('nieuwsFile', require('./config/nieuws.json'));
 app.set('categorieenFile', require('./config/categorieen.json'));
-
-
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static('public'))
 
+app.use(express.static('public'))
 app.use(require("./routes/root_router"));
 app.use(require("./routes/nieuws_router"));
 app.use(require("./routes/categorieen_router"));
